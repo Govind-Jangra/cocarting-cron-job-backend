b823e01e76533b78f6ce1838f841b292fa1bb816
Govind-Jangra <govind70821@gmail.com>
2024-10-16 18:53:55 +0530

    feat:working

--- index.js
+++ index.js
@@ -12,17 +12,11 @@ connectDB();
 app.use(express.json());
 async function fetchReducedHTML(url, timeout = 90000) {
     const browser = await puppeteer.launch({
-        args: [
-            "--disable-setuid-sandbox",
-            "--no-sandbox",
-            // "--single-process",
-            "--no-zygote",
-          ],
-          executablePath:
-            process.env.NODE_ENV === "production"
-              ? process.env.PUPPETEER_EXECUTABLE_PATH
-              : puppeteer.executablePath(),
-    });
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    })
+
+
 
     const page = await browser.newPage();
 
